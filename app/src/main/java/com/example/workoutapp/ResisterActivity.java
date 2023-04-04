bb7dd1c7f48b4664efdc2225829d953d306ca171
Park Min Woo <psmcm123@gmail.com>
2023-04-04 23:41:07 +0900

    ResisterActivity Commited

--- app/src/main/java/com/example/workoutapp/ResisterActivity.java
+++ app/src/main/java/com/example/workoutapp/ResisterActivity.java
@@ -2,13 +2,27 @@ package com.example.workoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ResisterActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resister);
+
+        ImageButton btnCancle = findViewById(R.id.btnCancle);
+
+        btnCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
