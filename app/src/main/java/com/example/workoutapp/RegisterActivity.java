2399161a57df350793f6053d3bf96d4da5af7bce
김재엽 <rlawo5219@naver.com>
2023-04-06 15:13:56 +0900

    Merge branch 'master' into JYK
    # Conflicts:
    #	app/src/main/java/com/example/workoutapp/RegisterActivity.java

--- app/src/main/java/com/example/workoutapp/RegisterActivity.java
+++ app/src/main/java/com/example/workoutapp/RegisterActivity.java
@@ -27,4 +27,23 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            Intent intent = new Intent(RegisterActivity.this,StartActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(R.anim.slide_left_enter,R.anim.slide_left_exit);
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish();
+            toast.cancel();
+        }
+    }
 }
