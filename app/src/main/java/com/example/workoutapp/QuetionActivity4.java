4183f05b08956d827b4566c139514fbed307fb4a
Miinuu <psmcm123@gmail.com>
2023-04-06 23:56:25 +0900

    Question Actvity UI committed

--- app/src/main/java/com/example/workoutapp/QuetionActivity4.java
+++ app/src/main/java/com/example/workoutapp/QuetionActivity4.java
@@ -2,13 +2,55 @@ package com.example.workoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class QuetionActivity4 extends AppCompatActivity {
+    private Button btnBirthday;
+    private Calendar calendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quetion4);
+
+        btnBirthday = findViewById(R.id.btnBirthday);
+        calendar = Calendar.getInstance();
+
+        btnBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    //btnBirthday 클릭 시 달력 다이어로그 보여줌
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
+                        btnBirthday.setText(selectedDate);
+                    }
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+            }
+        });
     }
 }
