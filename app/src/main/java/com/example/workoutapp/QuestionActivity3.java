4f2d3e8fcf96e13965b4b6c339d3dadf1284a4d0
Park Min Woo <psmcm123@gmail.com>
2023-04-07 00:53:11 +0900

    QuetionActivity4 Committed

--- app/src/main/java/com/example/workoutapp/QuestionActivity3.java
+++ app/src/main/java/com/example/workoutapp/QuestionActivity3.java
@@ -13,6 +13,7 @@ import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
 import android.text.style.TypefaceSpan;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 
 public class QuestionActivity3 extends AppCompatActivity {
 
+    //text 속성변경
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +55,47 @@ public class QuestionActivity3 extends AppCompatActivity {
         Spannable spanMaster = (Spannable) btnMaster.getText();
         spanMaster.setSpan(new RelativeSizeSpan(1.5f),2,4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spanMaster.setSpan(new StyleSpan(Typeface.BOLD),2,4,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    //첫번째 버튼 행동
+                    case R.id.btnBegginer:
+                        Intent intent1 = new Intent(QuestionActivity3.this,QuetionActivity4.class);
+                        startActivity(intent1);
+                        overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit);
+                        break;
+                    //두번째 버튼 행동
+                    case R.id.btnLow:
+                        Intent intent2 = new Intent(QuestionActivity3.this,QuetionActivity4.class);
+                        startActivity(intent2);
+                        overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit);
+                        break;
+                    case R.id.btnMiddle:
+                        Intent intent3 = new Intent(QuestionActivity3.this,QuetionActivity4.class);
+                        startActivity(intent3);
+                        overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit);
+                        break;
+                    case R.id.btnHigh:
+                        Intent intent4 = new Intent(QuestionActivity3.this,QuetionActivity4.class);
+                        startActivity(intent4);
+                        overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit);
+                        break;
+                    case R.id.btnMaster:
+                        Intent intent5 = new Intent(QuestionActivity3.this,QuetionActivity4.class);
+                        startActivity(intent5);
+                        overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit);
+                        break;
+                }
+            }
+        };
+
+        btnBegginer.setOnClickListener(onClickListener);
+        btnLow.setOnClickListener(onClickListener);
+        btnMiddle.setOnClickListener(onClickListener);
+        btnHigh.setOnClickListener(onClickListener);
+        btnMaster.setOnClickListener(onClickListener);
 
     }
 
--- app/src/main/java/com/example/workoutapp/QuetionActivity4.java
+++ app/src/main/java/com/example/workoutapp/QuetionActivity4.java
@@ -8,49 +8,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ProgressBar;
 
 import java.util.Calendar;
 
 public class QuetionActivity4 extends AppCompatActivity {
-    private Button btnBirthday;
-    private Calendar calendar;
-
+    Button btnBirthday;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quetion4);
 
+        ProgressBar pb = findViewById(R.id.progressbarPercent);
+        pb.setProgress(80);
+
         btnBirthday = findViewById(R.id.btnBirthday);
-        calendar = Calendar.getInstance();
 
-        btnBirthday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePickerDialog();
-            }
-        });
-    }
-
-    //btnBirthday 클릭 시 달력 다이어로그 보여줌
-    private void showDatePickerDialog(){
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
-                        btnBirthday.setText(selectedDate);
-                    }
-                },
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH));
-
-        datePickerDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                dialog.dismiss();
-            }
-        });
     }
 }
