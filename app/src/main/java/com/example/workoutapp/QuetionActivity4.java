800a560db1f72258c2dd07f3ee65391b9ade79ae
Park Min Woo <psmcm123@gmail.com>
2023-04-07 01:02:38 +0900

    QuetionActivity4 Committed

--- app/src/main/java/com/example/workoutapp/QuetionActivity4.java
+++ app/src/main/java/com/example/workoutapp/QuetionActivity4.java
@@ -26,5 +26,24 @@ public class QuetionActivity4 extends AppCompatActivity {
 
         btnBirthday = findViewById(R.id.btnBirthday);
 
+        btnBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(QuetionActivity4.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                btnBirthday.setText(year + "년" + (month+1) + "월" +dayOfMonth + "일");
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+
     }
 }
