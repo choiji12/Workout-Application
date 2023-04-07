e4ee48dfdfcc762de4a7fce1b81832f52c25b3d6
Park Min Woo <psmcm123@gmail.com>
2023-04-07 22:51:46 +0900

    btnSubmit Able,Disable Committed

--- app/src/main/java/com/example/workoutapp/QuestionActivity4.java
+++ app/src/main/java/com/example/workoutapp/QuestionActivity4.java
@@ -2,14 +2,20 @@ package com.example.workoutapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
+import android.text.method.DigitsKeyListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -53,8 +59,6 @@ public class QuestionActivity4 extends AppCompatActivity {
         EditText txtWeight = findViewById(R.id.txtWeight);
         EditText txtHeight = findViewById(R.id.txtHeight);
         Button btnSubmit = findViewById(R.id.btnSubmit);
-
-
         ImageButton btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,14 +80,82 @@ public class QuestionActivity4 extends AppCompatActivity {
             animation.setDuration(1000);
             animation.start();
         }
-        //정보 입력 안됐으면 버튼 비활성화
-        if(txtHeight.getText().toString().replace(" ","").equals("")||txtHeight.getText().equals("-")){
-            btnSubmit.setEnabled(false);
-        }
 
-        else if(txtHeight.getText().toString().replace(" ","").equals("")||txtHeight.getText().equals("-")){
-            btnSubmit.setEnabled(false);
-        }
+        //edittext 실수값만 입력 가능하게
+        txtHeight.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        txtWeight.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        txtHeight.setKeyListener(new DigitsKeyListener(false,true){
+            @Override
+            public int getInputType(){
+                return InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL;
+            }
+            @Override
+            protected char[] getAcceptedChars(){
+                return new char[]{'.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            }
+        });
+        txtWeight.setKeyListener(new DigitsKeyListener(false,true){
+            @Override
+            public int getInputType(){
+                return InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL;
+            }
+            @Override
+            protected char[] getAcceptedChars(){
+                return new char[]{'.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            }
+        });
+
+        //정보 입력 안됐으면 버튼 비활성화
+        Drawable enabledButtonBackground = ResourcesCompat.getDrawable(getResources(),R.drawable.roundshape_white_button,null);
+        Drawable disnabledButtonBackground = ResourcesCompat.getDrawable(getResources(),R.drawable.roundshape_grey_button,null);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // 문자열 비교를 위한 String 형식의 체중과 키
+                String weightString = txtWeight.getText().toString();
+                String heightString = txtHeight.getText().toString();
+                btnSubmit.setBackground(disnabledButtonBackground);
+                // "-"이거나 공백인 경우에 버튼 비활성화
+                if (weightString.equals("-") || heightString.equals("-") || weightString.trim().isEmpty() || heightString.trim().isEmpty()) {
+                    btnSubmit.setEnabled(false);
+                    btnSubmit.setBackground(disnabledButtonBackground);
+                } else {
+                    btnSubmit.setEnabled(true);
+                    btnSubmit.setBackground(enabledButtonBackground);
+                    btnSubmit.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.blue));
+                    // btnSubmit 클릭 시 버튼 background 변경
+                    btnSubmit.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            btnSubmit.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.blue));
+                        }
+                    });
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {       }
+        };
+
+        txtWeight.addTextChangedListener(textWatcher);
+        txtHeight.addTextChangedListener(textWatcher);
+
+        btnSubmit.setEnabled(false);
+        // btnSubmit 클릭 시 textcolor 변경
+        btnSubmit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (btnSubmit.isEnabled()) {
+                    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                        btnSubmit.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                    } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                        btnSubmit.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.blue));
+                    }
+                }
+                return false;
+            }
+        });
     }
     private long backKeyPressedTime = 0;
     private Toast toast;
