3a2e91e20b870036d9e2dd2d595daec1b61f7b5d
Park Min Woo <psmcm123@gmail.com>
2023-05-01 02:55:00 +0900

    Home Fragment Committed

--- app/src/main/java/com/example/workoutapp/AnalysisFragment.java
+++ app/src/main/java/com/example/workoutapp/AnalysisFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.github.mikephil.charting.animation.Easing;
@@ -73,77 +74,105 @@ public class AnalysisFragment extends Fragment {
     private RadioGroup peroid;
     private RadioGroup function;
     private LineChart chart;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_analysis, container,false);
 
-        chart = view.findViewById(R.id.chartAnalysis);
-
         peroid = view.findViewById(R.id.radioGroupPeriod);
         function = view.findViewById(R.id.radioGroupFunction);
+        chart = view.findViewById(R.id.chartAnalysis);
 
+        RadioButton btnWeek = view.findViewById(R.id.btnWeek);
+        RadioButton btnWeight = view.findViewById(R.id.btnWeight);
+        btnWeek.setChecked(true);
+        btnWeight.setChecked(true);
+
+        updateGraph(chart);
         peroid.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                updateGraph();
+                updateGraph(chart);
             }
         });
 
         function.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                updateGraph();
+                updateGraph(chart);
             }
         });
-
-        // 차트 체중 데이터
-
-
         return view;
     }
 
     /** 라디오 버튼 클릭 시 그래프를 업데이트 하는 함수 */
-    private void updateGraph() {
+    private void updateGraph(LineChart updateChart) {
         int peroidCheckedId= peroid.getCheckedRadioButtonId();
         int functionCheckedId = function.getCheckedRadioButtonId();
 
         if (peroidCheckedId == R.id.btnWeek && functionCheckedId == R.id.btnWeight) {
             // 주간 체중 변화량 그래프
             float[] weights = new float[]{(float) 43.3, (float) 44.1, (float) 43.2, (float) 42.6, (float) 42.4, (float) 43.2, (float) 43.6};
-            setWeekChart(chart,weights);
+            setChart(updateChart,weights,"week","weight");
         } else if (peroidCheckedId == R.id.btnMonth && functionCheckedId == R.id.btnWeight) {
             // 월간 체중 변화량 그래프
-            // ...
+            float[] weights = new float[]{(float) 43.3, (float) 41.1, (float) 39.5, (float) 42.2};
+            setChart(updateChart,weights,"month","weight");
         } else if (peroidCheckedId == R.id.btnYear && functionCheckedId == R.id.btnWeight) {
             // 연간 체중 변화량 그래프
-            //
+            float[] weights = new float[]{(float) 41.3, (float) 42.1, (float) 45.2, (float) 48.6, (float) 50.4, (float) 47.2,
+                    (float) 45.6,(float) 43.3, (float) 42.1, (float) 40.2, (float) 38.2, (float) 38.1, (float) 36.2};
+            setChart(updateChart,weights,"year","weight");
         } else if (peroidCheckedId == R.id.btnWeek && functionCheckedId == R.id.btnCalory) {
             // 주간 칼로리 소모량 그래프
-            // ...
+            float[] calorys = new float[]{(float) 440.3, (float) 400.1, (float) 500.2, (float) 350.6, (float) 210.4, (float) 750.2, (float) 100.6};
+            setChart(updateChart,calorys,"week","calory");
         } else if (peroidCheckedId == R.id.btnMonth && functionCheckedId == R.id.btnCalory) {
             // 월간 칼로리 소모량 그래프
-            // ...
+            float[] calorys = new float[]{(float) 400.2, (float) 341.1, (float) 239.5, (float) 742.2};
+            setChart(updateChart,calorys,"month","calory");
         } else if (peroidCheckedId == R.id.btnYear && functionCheckedId == R.id.btnCalory){
             // 연간 칼로리 소모량 그래프
+            float[] calorys = new float[]{(float) 501.3, (float) 302.1, (float) 445.2, (float) 148.6, (float) 30.4, (float) 600.2,
+                    (float) 800.6,(float) 900.3, (float) 1000.1, (float) 700.2, (float) 500.2, (float) 600.1, (float) 570.2};
+            setChart(updateChart,calorys,"year","calory");
         }
 
     }
-/** 일주일 체중 출력 선 그래프, 매개변수는 chart와 float[] */
-    private void setWeekChart(LineChart chart, float[] weights){
+    /** 사용자 분석 선 그래프, 매개변수는 chart와 일주일 간의 데이터를 저장하는 float[] , 기간을 지정하는 문자열, 기능을 지정하는 문자열 */
+    private void setChart(LineChart chart, float[] datas,String peroid,String function){
         List<Entry> values = new ArrayList<>();
-        for(int i=0; i<weights.length; i++){
-            values.add(new Entry(i,weights[i]));
-        }
-        //y축 범위 설정을 위한 체중 최댓값 최솟값
-        float maxWeight = Float.MIN_VALUE;
-        float minWeight = Float.MAX_VALUE;
-        for (float weight : weights) {
-            if (weight > maxWeight) {
-                maxWeight = weight;
+
+        float maxWeight = 0;
+        float minWeight = 0;
+        float maxCalory = 0;
+        float minCalory = 0;
+        if (function == "weight"){
+            for(int i=0; i<datas.length; i++) {
+                values.add(new Entry(i, datas[i]));
             }
-            if (weight < minWeight) {
-                minWeight = weight;
+//            y축 범위 설정을 위한 체중 최댓값 최솟값
+            minWeight = 200;
+            for (float weight : datas) {
+                if (weight > maxWeight) {
+                    maxWeight = weight;
+                }
+                if (weight < minWeight) {
+                    minWeight = weight;
+                }
+            }
+        } else if(function == "calory"){
+            for(int i =0; i<datas.length; i++){
+                values.add(new Entry(i,datas[i]));
+            }
+            for (float calory : datas){
+                if (calory > maxCalory){
+                    maxCalory = calory;
+                }
+                if (calory < minCalory){
+                    minCalory = calory;
+                }
             }
         }
 
@@ -165,13 +194,27 @@ public class AnalysisFragment extends Fragment {
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setGranularity(1f);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]
-                {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}));
+        if(peroid.equals("week") ) {
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]
+                    {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}));
+        } else if(peroid.equals("month")){
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]
+                    {"Week 1", "Week 2", "Week 3", "Week 4"}));
+        } else if(peroid.equals("year")){
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]
+                    {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+        }
 
         YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setAxisMinimum(minWeight-1);
-        leftAxis.setAxisMaximum(maxWeight+1);
-        leftAxis.setEnabled(false);
+        if(function.equals("weight")){
+            leftAxis.setAxisMinimum(minWeight-1);
+            leftAxis.setAxisMaximum(maxWeight+1);
+            leftAxis.setEnabled(false);
+        } else if(function.equals("calory")){
+            leftAxis.setAxisMinimum(minCalory-20);
+            leftAxis.setAxisMaximum(maxCalory+20);
+            leftAxis.setEnabled(false);
+        }
 
         YAxis rightAxis = chart.getAxisRight();
         rightAxis.setEnabled(false);
@@ -179,111 +222,6 @@ public class AnalysisFragment extends Fragment {
         chart.getDescription().setEnabled(false);
         chart.invalidate();
         chart.getLegend().setEnabled(false);
-        chart.animateX(1000,Easing.EaseInOutQuad);
-    }
-
-
-    private void setMonthChart(LineChart chart, float[] weights){
-        List<Entry> values = new ArrayList<>();
-        for(int i=0; i<weights.length; i++){
-            values.add(new Entry(i,weights[i]));
-        }
-        //y축 범위 설정을 위한 체중 최댓값 최솟값
-        float maxWeight = Float.MIN_VALUE;
-        float minWeight = Float.MAX_VALUE;
-        for (float weight : weights) {
-            if (weight > maxWeight) {
-                maxWeight = weight;
-            }
-            if (weight < minWeight) {
-                minWeight = weight;
-            }
-        }
-
-        LineDataSet dataSet = new LineDataSet(values,"weight");
-        dataSet.setColor(Color.parseColor("#6C91FA"));
-        dataSet.setCircleColor(Color.BLACK);
-        dataSet.setCircleHoleColor(Color.parseColor("#6C91FA"));
-        dataSet.setValueTextColor(Color.BLACK);
-        dataSet.setLineWidth(1.5f);
-        dataSet.enableDashedLine(10f,5f,0f);
-        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        dataSet.setCubicIntensity(0.2f);
-        dataSet.setDrawFilled(true);
-        dataSet.setFillDrawable(ContextCompat.getDrawable(getContext(), R.drawable.main_gradation_color_alpha50));
-        dataSet.setValueTextSize(10);
-        LineData lineData = new LineData(dataSet);
-        chart.setData(lineData);
-
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1f);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]
-                {"Week 1", "Week 2", "Week 3", "Week 4"}));
-
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setAxisMinimum(minWeight-1);
-        leftAxis.setAxisMaximum(maxWeight+1);
-        leftAxis.setEnabled(false);
-
-        YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-        chart.getDescription().setEnabled(false);
-        chart.invalidate();
-        chart.getLegend().setEnabled(false);
-        chart.animateX(1000,Easing.EaseInOutQuad);
-    }
-
-    private void setYearChart(LineChart chart, float[] weights){
-        List<Entry> values = new ArrayList<>();
-        for(int i=0; i<weights.length; i++){
-            values.add(new Entry(i,weights[i]));
-        }
-        //y축 범위 설정을 위한 체중 최댓값 최솟값
-        float maxWeight = Float.MIN_VALUE;
-        float minWeight = Float.MAX_VALUE;
-        for (float weight : weights) {
-            if (weight > maxWeight) {
-                maxWeight = weight;
-            }
-            if (weight < minWeight) {
-                minWeight = weight;
-            }
-        }
-
-        LineDataSet dataSet = new LineDataSet(values,"weight");
-        dataSet.setColor(Color.parseColor("#6C91FA"));
-        dataSet.setCircleColor(Color.BLACK);
-        dataSet.setCircleHoleColor(Color.parseColor("#6C91FA"));
-        dataSet.setValueTextColor(Color.BLACK);
-        dataSet.setLineWidth(1.5f);
-        dataSet.enableDashedLine(10f,5f,0f);
-        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        dataSet.setCubicIntensity(0.2f);
-        dataSet.setDrawFilled(true);
-        dataSet.setFillDrawable(ContextCompat.getDrawable(getContext(), R.drawable.main_gradation_color_alpha50));
-        dataSet.setValueTextSize(10);
-        LineData lineData = new LineData(dataSet);
-        chart.setData(lineData);
-
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1f);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]
-                {"1", "2", "3", "4", "5", "6", "7","1", "8", "9", "10", "11", "12"}));
-
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setAxisMinimum(minWeight-1);
-        leftAxis.setAxisMaximum(maxWeight+1);
-        leftAxis.setEnabled(false);
-
-        YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-        chart.getDescription().setEnabled(false);
-        chart.invalidate();
-        chart.getLegend().setEnabled(false);
-        chart.animateX(1000,Easing.EaseInOutQuad);
+        chart.animateY(1000);
     }
 }
