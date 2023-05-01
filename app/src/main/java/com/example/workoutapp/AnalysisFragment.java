fbd9f6643b93a8f84fb326779c9c331677977b65
Miinuu <psmcm123@gmail.com>
2023-05-01 17:25:40 +0900

    MainActivity Committed

--- app/src/main/java/com/example/workoutapp/AnalysisFragment.java
+++ app/src/main/java/com/example/workoutapp/AnalysisFragment.java
@@ -44,14 +44,6 @@ public class AnalysisFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AnalysisFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static AnalysisFragment newInstance(String param1, String param2) {
         AnalysisFragment fragment = new AnalysisFragment();
