385c2dc712c6b92d45c0f05006bfd4bb5a7ebeb8
Miinuu <psmcm123@gmail.com>
2023-05-10 20:32:08 +0900

    TimerActivity.java committed

--- app/src/main/java/com/example/workoutapp/SelectExercise.java
+++ app/src/main/java/com/example/workoutapp/SelectExercise.java
@@ -6,12 +6,15 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +28,17 @@ public class SelectExercise extends AppCompatActivity implements View.OnClickLis
     private int selectedmonth;
     private int seletedday;
     private String dateFor;
+
+    private RadioButton btnLeg;
+    private RadioButton btnChest;
+    private RadioButton btnBack;
+    private RadioButton btnShoulder;
+    private RadioButton btnCardio;
+    private RadioButton btnAbs;
+
+    private LinearLayout contentsLayout;
+
+    private CheckBox chkExercise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,27 +57,33 @@ public class SelectExercise extends AppCompatActivity implements View.OnClickLis
 
         Typeface mainFont = getResources().getFont(R.font.jamsil_regular);
 
-        int exerciseLength = 30;
+        int exerciseLength = 100;
+
         LinearLayout mainLayout = findViewById(R.id.exerciseLayout);
-//        LinearLayout contentsLayout = findViewById(R.id.contentsLayout);
 
-        for(int i=0; i<exerciseLength; i++){
-            LinearLayout contentsLayout = new LinearLayout(this);
+        btnLeg = findViewById(R.id.btnLeg);
+        btnChest = findViewById(R.id.btnChest);
+        btnBack = findViewById(R.id.btnBack);
+        btnShoulder = findViewById(R.id.btnShoulder);
+        btnCardio = findViewById(R.id.btnCardio);
+        btnAbs = findViewById(R.id.btnAbs);
+
+        for(int i=0; i<exerciseLength; i++) {
+            contentsLayout = new LinearLayout(this);
             contentsLayout.setOrientation(LinearLayout.HORIZONTAL);
-            contentsLayout.setId(i+101);
+            contentsLayout.setId(i + 101);
 
-            CheckBox chkExercise = new CheckBox(this);
-            chkExercise.setId(i+1);
-            chkExercise.setText("\t"+Integer.toString(i));
+            chkExercise = new CheckBox(this);
+            chkExercise.setId(i + 1);
+            chkExercise.setText("\t" + Integer.toString(i));
             chkExercise.setTextColor(getResources().getColorStateList(R.drawable.black_to_white_radio_text));
             chkExercise.setTypeface(mainFont);
             chkExercise.setTextSize(15);
             chkExercise.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-//            chkExercise.setBackground(getResources().getDrawable(R.drawable.rounded_grey_radiobutton));
 //            checkBox 크기 설정
             LinearLayout.LayoutParams paramsExercise = new LinearLayout.LayoutParams
-                    ((int) (350 * getResources().getDisplayMetrics().density),(int) (80 * getResources().getDisplayMetrics().density));
-            paramsExercise.setMargins(5,5,5,5);
+                    ((int) (350 * getResources().getDisplayMetrics().density), (int) (80 * getResources().getDisplayMetrics().density));
+            paramsExercise.setMargins(5, 5, 5, 5);
             chkExercise.setLayoutParams(paramsExercise);
 
             contentsLayout.addView(chkExercise);
@@ -71,32 +91,25 @@ public class SelectExercise extends AppCompatActivity implements View.OnClickLis
             Button btnExplain = new Button(this);
             contentsLayout.addView(btnExplain);
 
-            btnExplain.setId(i+10001);
+            btnExplain.setId(i + 10001);
             btnExplain.setBackground(getResources().getDrawable(R.drawable.info));
-//            btnExplain.setAllCaps(false);
-//            btnExplain.setText("i");
             btnExplain.setTextSize(12f);
 
-            // 테스트 ------------------------------------------------------------------------------------------
             btnExplain.setOnClickListener(this);
 
-            // 테스트 ------------------------------------------------------------------------------------------
-
             LinearLayout.LayoutParams paramsExplain = new LinearLayout.LayoutParams
-                    ((int) (30 * getResources().getDisplayMetrics().density),(int) (30 * getResources().getDisplayMetrics().density));
+                    ((int) (30 * getResources().getDisplayMetrics().density), (int) (30 * getResources().getDisplayMetrics().density));
             paramsExplain.gravity = Gravity.CENTER;
             btnExplain.setLayoutParams(paramsExplain);
 
             chkExercise.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if(isChecked){
+                    if (isChecked) {
                         contentsLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
-//                        btnExplain.setBackground(getResources().getDrawable(R.drawable.explan_circle_checked));
 
                     } else {
                         contentsLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
-//                        btnExplain.setBackground(getResources().getDrawable(R.drawable.explan_circle_unchecked));
                     }
                 }
             });
@@ -106,15 +119,205 @@ public class SelectExercise extends AppCompatActivity implements View.OnClickLis
                     (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
             paramsContents.setMargins(10, 5, 10, 5);
             contentsLayout.setLayoutParams(paramsContents);
-
-//            chkExercise.setButtonDrawable(android.R.color.transparent);
+            contentsLayout.setVisibility(View.VISIBLE);
             mainLayout.addView(contentsLayout);
+
         }
 
+        for (int j = 1; j < 101; j++) {
+            int id = j + 100;
+            LinearLayout LegLayout = (LinearLayout) findViewById(id);
+            Log.d("Leg Id", "LEGID :" +LegLayout.getId());
+            LegLayout.setVisibility(View.VISIBLE);
 
+            int chkId = j;
+            CheckBox checkBox = findViewById(chkId);
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                    } else {
+                        LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                    }
+                }
+            });
+        }
+
+        btnLeg.setOnClickListener(rdoScreen);
+        btnChest.setOnClickListener(rdoScreen);
+        btnBack.setOnClickListener(rdoScreen);
+        btnShoulder.setOnClickListener(rdoScreen);
+        btnCardio.setOnClickListener(rdoScreen);
+        btnAbs.setOnClickListener(rdoScreen);
     }
 
-    // 테스트 ------------------------------------------------------------------------------------------
+    View.OnClickListener rdoScreen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int LegLength = 20;
+            int BackLength = 20;
+            int ChestLength = 20;
+            int ShoulderLength = 20;
+            int CardioLength = 10;
+            int AbsLength = 10;
+            /** 하체, 등 버튼 기능 추가 */
+            if (btnLeg.isChecked()) {
+                for (int i=101; i<201; i++){
+                    LinearLayout layout = (LinearLayout) findViewById(i);
+                    layout.setVisibility(View.GONE);
+                }
+                for (int j = 0; j < LegLength; j++) {
+                    int id = j + 101;
+                    LinearLayout LegLayout = (LinearLayout) findViewById(id);
+                    Log.d("Leg Id", "LEGID :" +LegLayout.getId());
+                    LegLayout.setVisibility(View.VISIBLE);
+
+                    int chkId = j+101-100;
+                    CheckBox checkBox = findViewById(chkId);
+                    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                            } else {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                            }
+                        }
+                    });
+                }
+            }
+            else if (btnChest.isChecked()) {
+                for (int i=101; i<201; i++){
+                    LinearLayout layout = (LinearLayout) findViewById(i);
+                    layout.setVisibility(View.GONE);
+                }
+                for (int j = LegLength; j < LegLength + ChestLength; j++) {
+                    int id = j + 101;
+                    LinearLayout LegLayout = (LinearLayout) findViewById(id);
+                    LegLayout.setVisibility(View.VISIBLE);
+
+                    int chkId = j+101-100;
+                    CheckBox checkBox = findViewById(chkId);
+                    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                            } else {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                            }
+                        }
+                    });
+                }
+            }
+            else if (btnBack.isChecked()) {
+                for (int i=101; i<201; i++){
+                    LinearLayout layout = (LinearLayout) findViewById(i);
+                    layout.setVisibility(View.GONE);
+                }
+                for (int j = (LegLength + ChestLength); j < LegLength + BackLength + ChestLength; j++) {
+                    int id = j + 101;
+                    LinearLayout LegLayout = (LinearLayout)findViewById(id);
+                    LegLayout.setVisibility(View.VISIBLE);
+
+                    int chkId = j+101-100;
+                    CheckBox checkBox = findViewById(chkId);
+                    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                            } else {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                            }
+                        }
+                    });
+                }
+            }
+            else if (btnShoulder.isChecked()) {
+                for (int i=101; i<201; i++){
+                    LinearLayout layout = (LinearLayout) findViewById(i);
+                    layout.setVisibility(View.GONE);
+                }
+                for (int j = (LegLength + BackLength + ChestLength); j < LegLength + BackLength + ChestLength + ShoulderLength; j++) {
+                    int id = j + 101;
+                    LinearLayout LegLayout = (LinearLayout)findViewById(id);
+                    LegLayout.setVisibility(View.VISIBLE);
+
+                    int chkId = j+101-100;
+                    CheckBox checkBox = findViewById(chkId);
+                    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                            } else {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                            }
+                        }
+                    });
+                }
+            }
+            else if (btnCardio.isChecked()) {
+                for (int i=101; i<201; i++){
+                    LinearLayout layout = (LinearLayout) findViewById(i);
+                    layout.setVisibility(View.GONE);
+                }
+                for (int j = (LegLength + BackLength + ChestLength + ShoulderLength); j < LegLength + BackLength + ChestLength + ShoulderLength + CardioLength; j++) {
+                    int id = j + 101;
+                    LinearLayout LegLayout = (LinearLayout)findViewById(id);
+                    LegLayout.setVisibility(View.VISIBLE);
+
+                    int chkId = j+101-100;
+                    CheckBox checkBox = findViewById(chkId);
+                    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                            } else {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                            }
+                        }
+                    });
+                }
+            }
+            else if (btnAbs.isChecked()) {
+                for (int i=101; i<201; i++){
+                    LinearLayout layout = (LinearLayout) findViewById(i);
+                    layout.setVisibility(View.GONE);
+                }
+                for (int j = (LegLength + BackLength + ChestLength + ShoulderLength +CardioLength); j < LegLength + BackLength + ChestLength + ShoulderLength + CardioLength +AbsLength; j++) {
+                    int id = j + 101;
+                    LinearLayout LegLayout = (LinearLayout)findViewById(id);
+                    LegLayout.setVisibility(View.VISIBLE);
+
+                    int chkId = j+101-100;
+                    CheckBox checkBox = findViewById(chkId);
+                    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (isChecked) {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_checked));
+
+                            } else {
+                                LegLayout.setBackground(getResources().getDrawable(R.drawable.exercise_chkbox_unchecked));
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    };
+
+    /** 슬라이딩 업 , 운동 설명과 운동DB */
     @Override
     public void onClick(View view){
         int idx = 10001;
@@ -122,9 +325,8 @@ public class SelectExercise extends AppCompatActivity implements View.OnClickLis
             if(view.getId() == idx){
                 int ID = idx - 10000;
                 CheckBox checkBox = (CheckBox) findViewById(ID);
-                TextView tv = findViewById(R.id.tv_text1);
+                TextView tv = findViewById(R.id.txtExerciseName);
                 tv.setText(checkBox.getText());
-
 
                 SlidingUpPanelLayout slidingLayout = findViewById(R.id.main_frame);
 
@@ -135,7 +337,6 @@ public class SelectExercise extends AppCompatActivity implements View.OnClickLis
             idx++;
         }
     }
-    // 테스트 ------------------------------------------------------------------------------------------
 
     /** 뒤로가기 버튼 기능 구현 */
     private long backKeyPressedTime = 0;
