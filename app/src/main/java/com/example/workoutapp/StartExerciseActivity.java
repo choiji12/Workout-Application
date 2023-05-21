1854ec93894fead3dbc730368358143e36b275d1
Park Min Woo <psmcm123@gmail.com>
2023-05-21 23:31:02 +0900

    SelectExercise Comitted

--- app/src/main/java/com/example/workoutapp/StartExerciseActivity.java
+++ app/src/main/java/com/example/workoutapp/StartExerciseActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,27 +26,43 @@ public class StartExerciseActivity extends AppCompatActivity {
     private Button btnFinishSet;
     private TextView txtExerciseName;
     private LinearLayout mainLayout;
+    private TextView txtExerciseTime;
 
     private int exerciseLength;
     private int exerciseSequence=0;
     private int eachSetCount=0;
     private int exerciseFinishedCount=0;
     private int exerciseCount = 0;
+    private int minutes = 0;
+    private int seconds = 0;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_exercise);
+
+        /** 이전 엑티비티에서 데이터 호출 */
         getIntentData();
+
+        /** 뷰 초기화 */
         initView();
         exerciseLength = selectedExercise.size();
 
+        /** 운동 시작 뷰 추가 */
         addExerciseView(exerciseSequence);
+
+        startTimer();
+
+        /** 운동 완료버튼 눌렀을 때 동작 */
         btnFinishSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /** 세트 완료 시 동작 */
                 finishSet(exerciseSequence);
+                /** 완료한 세트 색깔 변경 */
                 finishedSetDecorator();
+                /** 모든 운동 완료 시 엑티비티 종료 */
                 if(exerciseFinishedCount == exerciseLength) {
                     finishActivity();
                 }
@@ -122,11 +140,35 @@ public class StartExerciseActivity extends AppCompatActivity {
 
     }
 
-
     private void initView(){
         mainLayout = findViewById(R.id.mainLayout);
         txtExerciseName = findViewById(R.id.txtExerciseName);
         btnFinishSet = findViewById(R.id.btnFinishSet);
+        txtExerciseTime = findViewById(R.id.txtExerciseTime);
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+    private void startTimer() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 1초마다 실행되는 작업
+                seconds++;
+                if (seconds == 60) {
+                    minutes++;
+                    seconds = 0;
+                }
+                String time = String.format("%02d:%02d", minutes, seconds);
+                txtExerciseTime.setText("운동 시간 : " + time);
+                handler.postDelayed(this, 1000); // 1초 후에 다시 실행
+            }
+        }, 1000); // 초기 1초 후에 실행
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 액티비티가 종료될 때 타이머 중지
+        handler.removeCallbacksAndMessages(null);
     }
 
     private void finishActivity(){
