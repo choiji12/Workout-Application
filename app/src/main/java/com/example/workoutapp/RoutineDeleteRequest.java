ea65952d5d535aa431cc92e0badd5acf58e49177
김재엽 <rlawo5219@naver.com>
2023-05-24 16:46:03 +0900

    commit JYK

---
+++ app/src/main/java/com/example/workoutapp/RoutineDeleteRequest.java
@@ -0,0 +1,29 @@
+package com.example.workoutapp;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RoutineDeleteRequest extends StringRequest {
+    final static private String URL = "http://wlgur0914.dothome.co.kr/RoutineDelete.php";
+    private Map<String, String> map;
+
+
+    public RoutineDeleteRequest(String routineName, String userID, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("routineName", routineName);
+        map.put("userID",userID);
+
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
